ac238d18fa16eaadad8cd523f455fac7b215c94e
liaoyustudent <wb-ly617236@alibaba-inc.com>
2020-02-11 17:43:59 +0800

    add function AssertAsMap

--- csharp/tests/CommonTest.cs
+++ csharp/tests/CommonTest.cs
@@ -1,8 +1,11 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
 
 using AlibabaCloud.TeaUtil;
+
+using Newtonsoft.Json.Linq;
 
 using Xunit;
 
@@ -184,8 +187,19 @@ namespace tests
         public void Test_StringifyMapValue()
         {
             Assert.Equal(new Dictionary<string, string>(), Common.StringifyMapValue(new Dictionary<string, object>()));
-            Assert.Equal("100", Common.StringifyMapValue(new Dictionary<string, object> { { "number", 100 } })["number"]);
+            Assert.Equal("100", Common.StringifyMapValue(new Dictionary<string, object> { { "number", 100 } }) ["number"]);
             Assert.Null(Common.StringifyMapValue(null));
+        }
+
+        [Fact]
+        public void Test_AssertAsMap()
+        {
+            Assert.Throws<ArgumentException>(() => { Common.AssertAsMap(null); });
+            string jsonStr = "{\"arrayObj\":[[{\"itemName\":\"item\",\"itemInt\":1},{\"itemName\":\"item2\",\"itemInt\":2}],[{\"itemName\":\"item3\",\"itemInt\":3}]],\"arrayList\":[[[1,2],[3,4]],[[5,6],[7]],[]],\"listStr\":[1,2,3],\"items\":[{\"total_size\":18,\"partNumber\":1,\"tags\":[{\"aa\":\"11\"}]},{\"total_size\":20,\"partNumber\":2,\"tags\":[{\"aa\":\"22\"}]}],\"next_marker\":\"\",\"test\":{\"total_size\":19,\"partNumber\":1,\"tags\":[{\"aa\":\"11\"}]}}";
+            JObject obj = JObject.Parse(jsonStr);
+            Dictionary<string, object> dict = Common.AssertAsMap(obj);
+            Assert.NotNull(dict);
+            Assert.Equal(6, dict.Count);
         }
     }
 }
