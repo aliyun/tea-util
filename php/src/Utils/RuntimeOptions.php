f67825fc746152d72ec8281e2ff27074d4d9a812
wb-hx510875 <wb-hx510875@alibaba-inc.com>
2020-03-17 10:41:18 +0800

    php: add RuntimeOptions Model

---
+++ php/src/Utils/RuntimeOptions.php
@@ -0,0 +1,40 @@
+<?php
+
+// This file is auto-generated, don't edit it. Thanks.
+
+namespace alibabacloud\sdk\Models;
+
+use AlibabaCloud\Tea\Model;
+
+class RuntimeOptions extends Model{
+    protected $_name = [];
+
+    public $autoretry;
+
+    public $ignoreSSL;
+
+    public $maxAttempts;
+
+    public $backoffPolicy;
+
+    public $backoffPeriod;
+
+    public $readTimeout;
+
+    public $connectTimeout;
+
+    public $httpProxy;
+
+    public $httpsProxy;
+
+    public $noProxy;
+
+    public $maxIdleConns;
+
+    public $localAddr;
+
+    public $socks5Proxy;
+
+    public $socks5NetWork;
+
+}
