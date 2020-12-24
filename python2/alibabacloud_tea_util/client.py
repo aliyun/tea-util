18fc2a83ab2f547238fa69f51dc946409b337506
824750130 <e824750130@qq.com>
2020-12-24 15:46:26 +0800

    support python2

---
+++ python2/alibabacloud_tea_util/client.py
@@ -0,0 +1,354 @@
+# -*- coding: utf-8 -*-
+import json
+import uuid
+import platform
+import socket
+import time
+from datetime import datetime
+from urllib import urlencode
+
+from Tea.converter import TeaConverter
+from Tea.model import TeaModel
+from Tea.stream import READABLE
+
+
+class Client(object):
+    """
+    This is a utility module
+    """
+
+    @staticmethod
+    def __read_part(f, size=1024):
+        while True:
+            part = f.read(size)
+            if part:
+                yield part
+            else:
+                return
+
+    @staticmethod
+    def __get_default_agent():
+        return 'AlibabaCloud (%s) python/%s TeaDSL/1' % (
+            platform.platform(), platform.python_version()
+        )
+
+    @staticmethod
+    def to_bytes(val):
+        """
+        Convert a string(utf8) to bytes
+        @return: the return bytes
+        """
+        return TeaConverter.to_str(val)
+
+    @staticmethod
+    def to_string(val):
+        """
+        Convert a bytes to string(utf8)
+        @return: the return string
+        """
+        return TeaConverter.to_unicode(val)
+
+    @staticmethod
+    def parse_json(val):
+        """
+        Parse it by JSON format
+        @return: the parsed result
+        """
+        try:
+            return json.loads(val)
+        except ValueError:
+            raise RuntimeError('Failed to parse the value as json format, Value: "%s".' % TeaConverter.to_str(val))
+
+    @staticmethod
+    def read_as_bytes(stream):
+        """
+        Read data from a readable stream, and compose it to a bytes
+        @param stream: the readable stream
+        @return: the bytes result
+        """
+        if isinstance(stream, str):
+            return stream
+        elif isinstance(stream, unicode):
+            return stream.encode('utf-8')
+        else:
+            b = ''
+            for part in Client.__read_part(stream, 1024):
+                b += part
+            return b
+
+    @staticmethod
+    def read_as_string(stream):
+        """
+        Read data from a readable stream, and compose it to a string
+        @param stream: the readable stream
+        @return: the string result
+        """
+        buff = Client.read_as_bytes(stream)
+        return Client.to_string(buff)
+
+    @staticmethod
+    def read_as_json(stream):
+        """
+        Read data from a readable stream, and parse it by JSON format
+        @param stream: the readable stream
+        @return: the parsed result
+        """
+        return Client.parse_json(Client.read_as_string(stream))
+
+    @staticmethod
+    def get_nonce():
+        """
+        Generate a nonce string
+        @return: the nonce string
+        """
+        name = socket.gethostname() + str(uuid.uuid1())
+        namespace = uuid.NAMESPACE_URL
+        return str(uuid.uuid5(namespace, name))
+
+    @staticmethod
+    def get_date_utcstring():
+        """
+        Get an UTC format string by current date, e.g. 'Thu, 06 Feb 2020 07:32:54 GMT'
+        @return: the UTC format string
+        """
+        return datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')
+
+    @staticmethod
+    def default_string(real, default):
+        """
+        If not set the real, use default value
+        @return: the return string
+        """
+        return real if real is not None else default
+
+    @staticmethod
+    def default_number(real, default):
+        """
+        If not set the real, use default value
+        @return: the return number
+        """
+        return real if real is not None else default
+
+    @staticmethod
+    def to_form_string(val):
+        """
+        Format a map to form string, like a=a%20b%20c
+        @return: the form string
+        """
+        if not val:
+            return ""
+        keys = sorted(list(val))
+        dic = [(k, val[k]) for k in keys]
+        return urlencode(dic)
+
+    @staticmethod
+    def to_jsonstring(val):
+        """
+        Stringify a value by JSON format
+        @return: the JSON format string
+        """
+        return json.dumps(val)
+
+    @staticmethod
+    def empty(val):
+        """
+        Check the string is empty?
+        @return: if string is null or zero length, return true
+        """
+        return not val
+
+    @staticmethod
+    def equal_string(val1, val2):
+        """
+        Check one string equals another one?
+        @return: if equals, return true
+        """
+        return val1 == val2
+
+    @staticmethod
+    def equal_number(val1, val2):
+        """
+        Check one number equals another one?
+        @return: if equals, return true
+        """
+        return val1 == val2
+
+    @staticmethod
+    def is_unset(value):
+        """
+        Check one value is unset
+        @return: if unset, return true
+        """
+        return value is None
+
+    @staticmethod
+    def stringify_map_value(m):
+        """
+        Stringify the value of map
+        @return: the new stringified map
+        """
+        if m is None:
+            return {}
+
+        dic_result = {}
+        for k, v in m.items():
+            if v is not None:
+                v = TeaConverter.to_unicode(v)
+            dic_result[k] = v
+        return dic_result
+
+    @staticmethod
+    def anyify_map_value(m):
+        """
+        Anyify the value of map
+        @return: the new anyfied map
+        """
+        return m
+
+    @staticmethod
+    def assert_as_boolean(value):
+        """
+        Assert a value, if it is a boolean, return it, otherwise throws
+        @return: the boolean value
+        """
+        if not isinstance(value, bool):
+            raise ValueError('%s is not a bool' % TeaConverter.to_str(value))
+        return value
+
+    @staticmethod
+    def assert_as_string(value):
+        """
+        Assert a value, if it is a string, return it, otherwise throws
+        @return: the string value
+        """
+        if not isinstance(value, unicode):
+            raise ValueError('%s is not a unicode' % TeaConverter.to_str(value))
+        return value
+
+    @staticmethod
+    def assert_as_bytes(value):
+        """
+        Assert a value, if it is a bytes, return it, otherwise throws
+        @return: the bytes value
+        """
+        if not isinstance(value, str):
+            raise ValueError('%s is not a str' % TeaConverter.to_str(value))
+        return value
+
+    @staticmethod
+    def assert_as_number(value):
+        """
+        Assert a value, if it is a number, return it, otherwise throws
+        @return: the number value
+        """
+        number = (int, float, long)
+        if not isinstance(value, number):
+            raise ValueError('%s is not a number' % TeaConverter.to_str(value))
+        return value
+
+    @staticmethod
+    def assert_as_map(value):
+        """
+        Assert a value, if it is a map, return it, otherwise throws
+        @return: the map value
+        """
+        if not isinstance(value, dict):
+            raise ValueError('%s is not a dict' % TeaConverter.to_str(value))
+        return value
+
+    @staticmethod
+    def get_user_agent(user_agent):
+        """
+        Get user agent, if it userAgent is not null, splice it with defaultUserAgent and return, otherwise return defaultUserAgent
+        @return: the string value
+        """
+        if user_agent:
+            return '%s %s' % (
+                Client.__get_default_agent(), user_agent
+            )
+        return Client.__get_default_agent()
+
+    @staticmethod
+    def is_2xx(code):
+        """
+        If the code between 200 and 300, return true, or return false
+        @return: boolean
+        """
+        return 200 <= code < 300
+
+    @staticmethod
+    def is_3xx(code):
+        """
+        If the code between 300 and 400, return true, or return false
+        @return: boolean
+        """
+        return 300 <= code < 400
+
+    @staticmethod
+    def is_4xx(code):
+        """
+        If the code between 400 and 500, return true, or return false
+        @return: boolean
+        """
+        return 400 <= code < 500
+
+    @staticmethod
+    def is_5xx(code):
+        """
+        If the code between 500 and 600, return true, or return false
+        @return: boolean
+        """
+        return 500 <= code < 600
+
+    @staticmethod
+    def validate_model(m):
+        """
+        Validate model
+        @return: void
+        """
+        if isinstance(m, TeaModel):
+            m.validate()
+
+    @staticmethod
+    def to_map(in_):
+        """
+        Model transforms to map[string]any
+        @return: map[string]any
+        """
+        if isinstance(in_, TeaModel):
+            return in_.to_map()
+        else:
+            return in_
+
+    @staticmethod
+    def sleep(millisecond):
+        """
+        Suspends the current thread for the specified number of milliseconds.
+        """
+        time.sleep(millisecond / 1000)
+
+    @staticmethod
+    def to_array(input):
+        """
+        Transform input as array.
+        """
+        if input is None:
+            return []
+
+        out = []
+        for i in input:
+            if isinstance(i, TeaModel):
+                out.append(i.to_map())
+            else:
+                out.append(i)
+        return out
+
+    @staticmethod
+    def assert_as_readable(value):
+        """
+        Assert a value, if it is a readable, return it, otherwise throws
+        @return: the readable value
+        """
+        if not isinstance(value, READABLE):
+            raise ValueError('The value is not a readable')
+        return value
---
+++ python2/tests/test_client.py
@@ -0,0 +1,258 @@
+# -*- coding: utf-8 -*-
+import unittest
+import time
+import os
+
+from Tea.model import TeaModel
+from alibabacloud_tea_util.client import Client
+
+base_path = os.path.dirname(__file__)
+
+
+class TestClient(unittest.TestCase):
+
+    class TestModel(TeaModel):
+        def __init__(self):
+            self.test_a = 'a'
+            self.test_b = 'b'
+
+        def validate(self):
+            raise ValueError('test validate')
+
+        def to_map(self):
+            return {
+                'test_a': self.test_a,
+                'test_b': self.test_b
+            }
+
+    def test_to_bytes(self):
+        result = Client.to_bytes("test")
+        self.assertEqual(b'test', result)
+
+    def test_to_string(self):
+        self.assertEqual("test", Client.to_string(b'test'))
+
+    def test_parse_json(self):
+        json_str = '{"arrayObj":[[{"itemName":"item","itemInt":1},{"itemName":"item2","itemInt":2}],[{"itemName":"item3","itemInt":3}]],"arrayList":[[[1,2],[3,4]],[[5,6],[7]],[]],"listStr":[1,2,3],"items":[{"total_size":18,"partNumber":1,"tags":[{"aa":"11"}]},{"total_size":20,"partNumber":2,"tags":[{"aa":"22"}]}],"next_marker":"","test":{"total_size":19,"partNumber":1,"tags":[{"aa":"11"}]}}'
+        obj = Client.parse_json(json_str)
+        self.assertIsNotNone(obj)
+
+    def test_read_as_bytes(self):
+        self.assertEqual('test', Client.read_as_bytes('test'))
+        self.assertEqual('testStr', Client.read_as_bytes(u'testStr'))
+
+        filename = base_path + "/test_open.txt"
+        with open(filename, 'rb') as f:
+            result = Client.read_as_bytes(f)
+        self.assertIsNotNone(result)
+        self.assertEqual(b'hello alibabaCloud!', result)
+
+    def test_read_as_string(self):
+        module_path = os.path.dirname(__file__)
+        filename = module_path + "/test_open.txt"
+        with open(filename, 'rb') as f:
+            result = Client.read_as_string(f)
+        self.assertIsNotNone(result)
+        self.assertEqual('hello alibabaCloud!', result)
+        self.assertEqual('test', Client.read_as_string(b'test'))
+        self.assertEqual('testStr', Client.read_as_string('testStr'))
+
+    def test_read_as_json(self):
+        self.assertEqual({"key": "value"},
+                         Client.read_as_json('{"key":"value"}'))
+        try:
+            Client.read_as_json("{1:'2'}")
+            assert False
+        except Exception as e:
+            self.assertEqual('''Failed to parse the value as json format, Value: "{1:'2'}".''', str(e))
+
+    def test_get_nonce(self):
+        self.assertIsNotNone(Client.get_nonce())
+
+    def test_get_date_utc_string(self):
+        self.assertIn('GMT', Client.get_date_utcstring())
+
+    def test_default_string(self):
+        self.assertEqual("", Client.default_string("", "default"))
+        self.assertEqual("default", Client.default_string(None, "default"))
+        self.assertEqual("str", Client.default_string("str", "default"))
+
+    def test_default_number(self):
+        self.assertEqual(100, Client.default_number(None, 100))
+        self.assertEqual(0, Client.default_number(0, 100))
+        self.assertEqual(1, Client.default_number(1, 100))
+
+    def test_to_form_string(self):
+        self.assertEqual("", Client.to_form_string(None))
+        self.assertEqual("", Client.to_form_string({}))
+        dic = {}
+        dic["form"] = "test"
+        dic["param"] = "test"
+        self.assertEqual("form=test&param=test", Client.to_form_string(dic))
+
+    def test_to_json_string(self):
+        self.assertEqual('{"key": "value"}',
+                         Client.to_jsonstring({"key": "value"}))
+
+    def test_empty(self):
+        self.assertTrue(Client.empty(None))
+        self.assertTrue(Client.empty(""))
+        self.assertFalse(Client.empty("test"))
+
+    def test_equal_string(self):
+        self.assertTrue(Client.equal_string("test", "test"))
+        self.assertFalse(Client.equal_string("a", "b"))
+
+    def test_equal_number(self):
+        self.assertTrue(Client.equal_number(100, 100))
+        self.assertFalse(Client.equal_number(1, 2))
+
+    def test_is_unset(self):
+        self.assertTrue(Client.is_unset(None))
+        self.assertFalse(Client.is_unset(""))
+        self.assertFalse(Client.is_unset("11"))
+
+    def test_stringify_map_value(self):
+        self.assertEqual({}, Client.stringify_map_value(None))
+        self.assertEqual({}, Client.stringify_map_value({}))
+        dic = {
+            'test': 100,
+            'bkey': b'bytes',
+            'key': None
+        }
+        self.assertEqual("100", Client.stringify_map_value(dic)["test"])
+
+    def test_assert_as_map(self):
+        dic = {'key': 'value'}
+        s = 'test'
+        res = Client.assert_as_map(dic)
+        self.assertEqual(dic, res)
+        try:
+            Client.assert_as_map(s)
+            assert False
+        except ValueError as e:
+            self.assertEqual('test is not a dict', str(e))
+
+    def test_get_user_agent(self):
+        self.assertIsNotNone(Client.get_user_agent(''))
+        self.assertIn("test", Client.get_user_agent("test"))
+
+    def test_is_xx(self):
+        self.assertFalse(Client.is_2xx(100))
+        self.assertTrue(Client.is_2xx(200))
+        self.assertFalse(Client.is_2xx(300))
+
+        self.assertFalse(Client.is_3xx(200))
+        self.assertTrue(Client.is_3xx(300))
+        self.assertFalse(Client.is_3xx(400))
+
+        self.assertFalse(Client.is_4xx(300))
+        self.assertTrue(Client.is_4xx(400))
+        self.assertFalse(Client.is_4xx(500))
+
+        self.assertFalse(Client.is_5xx(400))
+        self.assertTrue(Client.is_5xx(500))
+        self.assertFalse(Client.is_5xx(600))
+
+    def test_anyify_map_value(self):
+        dic = {
+            'key': 'value'
+        }
+        self.assertEqual(dic, Client.anyify_map_value(dic))
+
+    def test_assert_as_bytes(self):
+        s = u'test'
+        b = 'test'
+        res = Client.assert_as_bytes(b)
+        self.assertEqual(b, res)
+        try:
+            Client.assert_as_bytes(s)
+            assert False
+        except ValueError as e:
+            self.assertEqual('test is not a str', str(e))
+
+    def test_assert_as_string(self):
+        num = 1
+        s = u'test'
+        res = Client.assert_as_string(s)
+        self.assertEqual(s, res)
+        try:
+            Client.assert_as_string(num)
+            assert False
+        except ValueError as e:
+            self.assertEqual('1 is not a unicode', str(e))
+
+    def test_assert_as_number(self):
+        s = 'test'
+        num = 1
+        res = Client.assert_as_number(num)
+        self.assertEqual(num, res)
+        try:
+            Client.assert_as_number(s)
+        except ValueError as e:
+            self.assertEqual('test is not a number', str(e))
+
+    def test_assert_as_boolean(self):
+        s = 'test'
+        boolean = True
+        res = Client.assert_as_boolean(boolean)
+        self.assertEqual(boolean, res)
+        try:
+            Client.assert_as_boolean(s)
+            assert False
+        except ValueError as e:
+            self.assertEqual('test is not a bool', str(e))
+
+    def test_validate_model(self):
+        model = {'t': 'ok'}
+        Client.validate_model(model)
+        model = self.TestModel()
+        try:
+            Client.validate_model(model)
+            assert False
+        except ValueError as e:
+            self.assertEqual('test validate', str(e))
+
+    def test_to_map(self):
+        model = {'t': 'ok'}
+        dic = Client.to_map(model)
+        self.assertEqual(dic, model)
+
+        model = self.TestModel()
+        dic = Client.to_map(model)
+        self.assertEqual(
+            'a',
+            dic.get('test_a')
+        )
+
+        self.assertEqual(
+            'b',
+            dic.get('test_b')
+        )
+
+    def test_sleep(self):
+        t1 = time.time()
+        Client.sleep(2000)
+        self.assertEqual(2, int(time.time() - t1))
+
+    def test_to_array(self):
+        tm = self.TestModel()
+        lis = [tm, tm]
+        res = Client.to_array(lis)
+        self.assertEqual('a', res[0]['test_a'])
+        res = Client.to_array(None)
+        self.assertEqual([], res)
+        lis = ['tm', 'tm']
+        res = Client.to_array(lis)
+        self.assertEqual(lis, res)
+
+    def test_assert_as_readable(self):
+        with open(os.path.join(base_path, 'test_open.txt'), 'rb') as f:
+            f = Client.assert_as_readable(f)
+            f.read()
+
+        try:
+            Client.assert_as_readable('readable')
+            assert False
+        except ValueError as e:
+            self.assertEqual('The value is not a readable', str(e))
